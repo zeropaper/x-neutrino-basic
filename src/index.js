78d60cdcb9d6c0cec4a5d152873120c1b08200cb
Valentin Vago <valentin.vago@gmail.com>
2017-11-22 15:42:46 +0100

    add kitten

--- src/index.js
+++ src/index.js
@@ -4,42 +4,64 @@ import './styles.scss';
 
 const navbar = `
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <a class="navbar-brand" href="#">MyApp</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <div class="collapse navbar-collapse" id="navbar">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Category 1 <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Category 2 <span class="sr-only">(current)</span></a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
 `;
 
+const pictures = [
+  'kitten-2.jpg',
+  'kitten-little.jpg',
+  'kitten.jpg',
+  'koala.jpg',
+  'pinguin.jpg',
+  'sloth.jpg',
+];
+
+function mkCard(img) {
+  return `
+<div class="card">
+  <img class="card-img-top" src="static/${img}" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+`;
+}
+
+const grid = `
+<div class="container-fluid">
+  <div class="row shop-products-row">
+    <div class="col-12 col-lg-4">${mkCard(pictures[0])}</div>
+    <div class="col-12 col-lg-4">${mkCard(pictures[1])}</div>
+    <div class="col-12 col-lg-4">${mkCard(pictures[2])}</div>
+  </div>
+  <div class="row shop-products-row">
+    <div class="col-12 col-lg-4">${mkCard(pictures[3])}</div>
+    <div class="col-12 col-lg-4">${mkCard(pictures[4])}</div>
+    <div class="col-12 col-lg-4">${mkCard(pictures[5])}</div>
+  </div>
+</div>
+`;
+
+
 $(() => {
-  $('#root').append(navbar);
+  $('#root')
+    .append(navbar)
+    .append(grid);
 });
